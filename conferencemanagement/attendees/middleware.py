8fc882d98a38a7ddf17e08ecfdc9de7b153ad66d
Mingze Ni <38578360+Andrevvdog@users.noreply.github.com>
2022-07-19 15:05:16 -0400

    Add files via upload

---
+++ conferencemanagement/attendees/middleware.py
@@ -0,0 +1,28 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+import re
+
+class Middleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+        print("MiddleWare")
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        path = request.path
+        print("url: ", path)
+
+        urllist = ['/attendees/login', '/attendees/dologin', '/attendees/logout']
+        if re.match(r'^/attendees', path) and (path not in urllist):
+            if 'attendeeuser' not in request.session:
+                return redirect(reverse("attendees_login"))
+
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
