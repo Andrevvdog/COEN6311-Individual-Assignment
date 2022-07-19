ab806d6d24e4f383bd0850f9815451a6c9ed4f2a
Mingze Ni <38578360+Andrevvdog@users.noreply.github.com>
2022-07-19 14:47:24 -0400

    Add files via upload

---
+++ attendees/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from attendees import views as attendees
+from conference import views as conference
+from events import views as events
+
+urlpatterns = [
+    path('<int:pIndex>', attendees.index, name = "attendees_index"),
+
+    path('login', attendees.login, name = "attendees_login"),
+    path('dologin', attendees.dologin, name = "attendees_dologin"),
+    path('logout', attendees.logout, name = "attendees_logout"),
+
+    path('attendees/register/<int:cid>', conference.register, name = "attendees_conference_register"),
+
+    path('events/<int:pIndex>', events.index, name = "attendees_events_index"),
+]
